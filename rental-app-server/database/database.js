f3cbc5ef111007c1d6537472ab4c0b641d3c55b0
deStarxis <humagainuzjwol@gmail.com>
2023-02-02 22:49:23 -0600

    backend initialization

---
+++ rental-app-server/database/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+module.exports.databaseConnection = async () => {
+  try {
+    mongoose.set("strictQuery", true);
+    const result = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected to : ${result.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
