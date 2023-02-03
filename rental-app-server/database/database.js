8a818a1f9c7c1d6ac652f04f377233af7f85edcb
deStarxis <humagainuzjwol@gmail.com>
2023-02-03 10:54:28 -0600

    updated db setup

--- rental-app-server/database/database.js
+++ rental-app-server/database/database.js
@@ -3,7 +3,8 @@ module.exports.databaseConnection = async () => {
   try {
     mongoose.set("strictQuery", true);
     const result = await mongoose.connect(
-      process.env.MONGO_URI || process.env.LOCAL_DATABASE_URI,
+      // process.env.MONGO_URI ||
+      process.env.LOCAL_DATABASE_URI,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
