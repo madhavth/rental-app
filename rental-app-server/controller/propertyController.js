5562d1c3472dcca24764d2d713f5c771f862ddc7
deStarxis <humagainuzjwol@gmail.com>
2023-02-03 17:09:57 -0600

    fixed conflicts

--- rental-app-server/controller/propertyController.js
+++ rental-app-server/controller/propertyController.js
@@ -1,153 +1,230 @@
-const Property = require('../model/propertyModel');
+const { uploadImage } = require("../config/cloudinary");
+const Property = require("../model/propertyModel");
 
 const defaultPerPage = 1;
 
 const getMetaData = async (req, res) => {
-    const page = req.query.page || 0;
-    const size = req.query.size || defaultPerPage;
+  const page = req.query.page || 0;
+  const size = req.query.size || defaultPerPage;
 
-    const totalDocuments = await Property.countDocuments({});
-    const remaining = totalDocuments - (((+page) + 1) * (+size));
+  const totalDocuments = await Property.countDocuments({});
+  const remaining = totalDocuments - (+page + 1) * +size;
 
-    return {
-        page: req.query.page || 0,
-        size: req.query.size || defaultPerPage,
-        total: await Property.countDocuments({}),
-        remaining: remaining > 0 ? remaining : 0
-    };
-}
-
-const paginatedResult = async (req, res, next, query) => {
-    const page = req.query.page || 0;
-    const size = req.query.size || defaultPerPage;
-
-    const properties = await Property.find(query, {
-        __v: 0
-    }).limit(size)
-        .skip(size * page);
-
-    return {
-        success: true,
-        data: {
-            properties: properties,
-            metadata: await getMetaData(req, res)
-        }
-    }
-}
-
-
-module.exports.getAllProperties = async (req, res, next) => {
-    try {
-
-        const result = await paginatedResult(req, res, next, {});
-        res.json(result);
-    } catch (e) {
-        next(new Error('Error while fetching all properties'));
-    }
+  return {
+    page: req.query.page || 0,
+    size: req.query.size || defaultPerPage,
+    total: await Property.countDocuments({}),
+    remaining: remaining > 0 ? remaining : 0,
+  };
 };
 
+const paginatedResult = async (req, res, next, query) => {
+  const page = req.query.page || 0;
+  const size = req.query.size || defaultPerPage;
+
+  const properties = await Property.find(query, {
+    __v: 0,
+  })
+    .limit(size)
+    .skip(size * page);
+
+  return {
+    success: true,
+    data: {
+      properties: properties,
+      metadata: await getMetaData(req, res),
+    },
+  };
+};
+
+module.exports.getAllProperties = async (req, res, next) => {
+  try {
+    const result = await paginatedResult(req, res, next, {});
+    res.json(result);
+  } catch (e) {
+    next(new Error("Error while fetching all properties"));
+  }
+};
 
 module.exports.getNearByProperties = async (req, res, next) => {
-    try {
-        const longitude = req.query.longitude;
-        const latitude = req.query.latitude;
+  try {
+    const longitude = req.query.longitude;
+    const latitude = req.query.latitude;
 
-        const query = {
-            location: {
-                $near: {
-                    $geometry: {
-                        type: "Point",
-                        coordinates: [longitude, latitude]
-                    },
-                    $maxDistance: 10000
-                }
-            }
-        };
+    const query = {
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        },
+      },
+    };
 
-        const results = await paginatedResult(req, res, next, query);
-        res.json(results);
-    } catch (e) {
-        next(new Error('Error while fetching nearby properties'));
-    }
-}
+    const results = await paginatedResult(req, res, next, query);
+    res.json(results);
+  } catch (e) {
+    next(new Error("Error while fetching nearby properties"));
+  }
+};
 
 module.exports.getTrendingProperties = async (req, res, next) => {
-    try {
-        const page = req.query.page || 0;
-        const size = req.query.size || defaultPerPage;
+  try {
+    const page = req.query.page || 0;
+    const size = req.query.size || defaultPerPage;
 
-        const properties = await Property.find({}, {
-            __v: 0
-        }).sort({view_count: -1})
-            .limit(size)
-            .skip(size * page);
-        res.json({success: true, data: properties, metadata: await getMetaData(req, res)});
-    } catch (e) {
-        next(new Error('Error while fetching most viewed properties'));
-    }
-}
-
+    const properties = await Property.find(
+      {},
+      {
+        __v: 0,
+      }
+    )
+      .sort({ view_count: -1 })
+      .limit(size)
+      .skip(size * page);
+    res.json({
+      success: true,
+      data: properties,
+      metadata: await getMetaData(req, res),
+    });
+  } catch (e) {
+    next(new Error("Error while fetching most viewed properties"));
+  }
+};
 
 module.exports.getPropertyById = async (req, res, next) => {
-    try {
-        const propertyId = req.params.property_id;
-        const property = await Property.findOne({
-            _id: propertyId
-        }, {
-            __v: 0
-        });
-        res.json({success: true, data: property});
-    } catch (e) {
-        next(new Error('Error while fetching property'));
-    }
-
-}
+  try {
+    const propertyId = req.params.property_id;
+    const property = await Property.findOne(
+      {
+        _id: propertyId,
+      },
+      {
+        __v: 0,
+      }
+    );
+    res.json({ success: true, data: property });
+  } catch (e) {
+    next(new Error("Error while fetching property"));
+  }
+};
 
 module.exports.deletePropertyById = async (req, res, next) => {
-    try {
-        const propertyId = req.params.property_id;
-        await Property.deleteOne({_id: propertyId});
-        res.json({success: true, message: 'Deleted property successfully'});
-    } catch (e) {
-        next(new Error('Error while deleting property'));
-    }
-
-}
+  try {
+    const propertyId = req.params.property_id;
+    await Property.deleteOne({ _id: propertyId });
+    res.json({ success: true, message: "Deleted property successfully" });
+  } catch (e) {
+    next(new Error("Error while deleting property"));
+  }
+};
 
 module.exports.addProperty = async (req, res, next) => {
-    try {
-        req.body.view_count = undefined;
+  try {
+    req.body.view_count = undefined;
 
-        const property = new Property(req.body);
-        await property.save();
-        res.json({
-            success: true, message: 'Added new property successfully'
-        })
-    } catch (e) {
-        next(new Error('Error while adding new property'));
+    const property = new Property(req.body);
+    await property.save();
+    res.json({
+      success: true,
+      message: "Added new property successfully",
+    });
+  } catch (e) {
+    next(new Error("Error while adding new property"));
+  }
+};
+
+module.exports.getPropertyById = async (req, res, next) => {
+  try {
+    const propertyId = req.params.property_id;
+    const property = await Property.findOne(
+      {
+        _id: propertyId,
+      },
+      {
+        __v: 0,
+      }
+    );
+    res.json({ success: true, data: property });
+  } catch (e) {
+    next(new Error("Error while fetching property "));
+  }
+};
+
+module.exports.deletePropertyById = async (req, res, next) => {
+  try {
+    const propertyId = req.params.property_id;
+    await Property.deleteOne({ _id: propertyId });
+    res.json({ success: true, message: "Deleted property successfully" });
+  } catch (e) {
+    next(new Error("Error while deleting property"));
+  }
+};
+
+module.exports.addProperty = async (req, res, next) => {
+  try {
+    const property = new Property(req.body);
+    await property.save();
+    res.json({
+      success: true,
+      message: "Added new property successfully",
+    });
+  } catch (e) {
+    next(new Error("Error while adding new property"));
+  }
+};
+
+module.exports.uploadPropertyImages = async (req, res, next) => {
+  try {
+    const { _id } = req.body;
+    let propertyImages = [];
+    if (req.files !== undefined) {
+      if (req.files.length > 0) {
+        for (var i = 0; i < req.files.length; i++) {
+          var locaFilePath = req.files[i].path;
+          var result = await uploadImage(locaFilePath);
+          propertyImages.push({ img: result.secure_url });
+        }
+      }
+      if (propertyImages.length > 0) {
+        await Property.findOneAndUpdate(
+          {
+            _id,
+          },
+          {
+            $set: { propertyImages },
+          }
+        );
+        res.json({ success: true, message: propertyImages });
+      }
     }
-}
-
+  } catch (error) {
+    next(error);
+  }
+};
 module.exports.updateProperty = async (req, res, next) => {
-    try {
+  try {
+    const id = req.params.property_id;
 
-        const id = req.params.property_id;
+    const updatedInfo = req.body;
 
-        const updatedInfo = req.body;
-
-        await Property.updateOne({
-            _id: id,
-        }, {
-            $set: {
-                ...req.body
-            }
-        });
-        res.json({
-            success: true,
-            message: `updated property successfully`
-        })
-    } catch (e) {
-        next(new Error('Error while updating property'));
-    }
-}
-
+    await Property.updateOne(
+      {
+        _id: id,
+      },
+      {
+        $set: {
+          ...req.body,
+        },
+      }
+    );
+    res.json({
+      success: true,
+      message: `updated property successfully`,
+    });
+  } catch (e) {
+    next(new Error("Error while updating property"));
+  }
+};
--- rental-app-server/route/propertyRoute.js
+++ rental-app-server/route/propertyRoute.js
@@ -1,22 +1,30 @@
-const express = require('express');
+const express = require("express");
+const { upload } = require("../config/multer");
 const router = express.Router();
 const {
-    getAllProperties,
-    getPropertyById,
-    deletePropertyById,
-    addProperty,
-    updateProperty,
-    getNearByProperties,
-    getTrendingProperties
-} = require('../controller/propertyController');
+  getAllProperties,
+  getPropertyById,
+  deletePropertyById,
+  addProperty,
+  updateProperty,
+  getNearByProperties,
+  getTrendingProperties,
+  uploadPropertyImages,
+} = require("../controller/propertyController");
 
-router.get('/', getAllProperties);
-router.get('/nearby', getNearByProperties);
-router.get('/trending', getTrendingProperties);
+router.get("/", getAllProperties);
+router.get("/nearby", getNearByProperties);
+router.get("/trending", getTrendingProperties);
 
-router.get('/:property_id', getPropertyById);
-router.delete('/:property_id', deletePropertyById);
-router.post('/', addProperty);
-router.patch('/:property_id', updateProperty);
+router.get("/:property_id", getPropertyById);
+router.delete("/:property_id", deletePropertyById);
+router.post("/", addProperty);
+router.patch("/:property_id", updateProperty);
+
+router.patch(
+  "/upload-property-images",
+  upload.array("image", 5),
+  uploadPropertyImages
+);
 
 module.exports = router;
