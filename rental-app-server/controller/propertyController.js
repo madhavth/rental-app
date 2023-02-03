e7c548e3e6082793d0ac509fc35bf2b3009d8e3c
madhavth <madhavth77@gmail.com>
2023-02-03 15:27:43 -0600

    added metadatas for pagination results

--- rental-app-server/controller/propertyController.js
+++ rental-app-server/controller/propertyController.js
@@ -1,23 +1,46 @@
 const Property = require('../model/propertyModel');
 
-const defaultPerPage = 20;
+const defaultPerPage = 1;
 
-const paginatedResult =async (req, res, next, query) => {
+const getMetaData = async (req, res) => {
     const page = req.query.page || 0;
     const size = req.query.size || defaultPerPage;
-    
-    return Property.find(query, {
+
+    const totalDocuments = await Property.countDocuments({});
+    const remaining = totalDocuments - (((+page) + 1) * (+size));
+
+    return {
+        page: req.query.page || 0,
+        size: req.query.size || defaultPerPage,
+        total: await Property.countDocuments({}),
+        remaining: remaining > 0 ? remaining : 0
+    };
+}
+
+const paginatedResult = async (req, res, next, query) => {
+    const page = req.query.page || 0;
+    const size = req.query.size || defaultPerPage;
+
+    const properties = await Property.find(query, {
         __v: 0
     }).limit(size)
         .skip(size * page);
+
+    return {
+        success: true,
+        data: {
+            properties: properties,
+            metadata: await getMetaData(req, res)
+        }
+    }
 }
 
 
 module.exports.getAllProperties = async (req, res, next) => {
     try {
-        
-        const properties = await paginatedResult(req, res, next, {});
-        res.json({success: true, data: properties});
+
+        const result = await paginatedResult(req, res, next, {});
+        res.json(result);
     } catch (e) {
         next(new Error('Error while fetching all properties'));
     }
@@ -28,7 +51,7 @@ module.exports.getNearByProperties = async (req, res, next) => {
     try {
         const longitude = req.query.longitude;
         const latitude = req.query.latitude;
-        
+
         const query = {
             location: {
                 $near: {
@@ -40,9 +63,9 @@ module.exports.getNearByProperties = async (req, res, next) => {
                 }
             }
         };
-        
-        const properties = await paginatedResult(req, res, next, query);
-        res.json({success: true, data: properties});
+
+        const results = await paginatedResult(req, res, next, query);
+        res.json(results);
     } catch (e) {
         next(new Error('Error while fetching nearby properties'));
     }
@@ -52,13 +75,13 @@ module.exports.getTrendingProperties = async (req, res, next) => {
     try {
         const page = req.query.page || 0;
         const size = req.query.size || defaultPerPage;
-        
+
         const properties = await Property.find({}, {
             __v: 0
         }).sort({view_count: -1})
             .limit(size)
             .skip(size * page);
-        res.json({success: true, data: properties});
+        res.json({success: true, data: properties, metadata: await getMetaData(req, res)});
     } catch (e) {
         next(new Error('Error while fetching most viewed properties'));
     }
