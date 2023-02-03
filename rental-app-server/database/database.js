94c3c2f548bd08df490361c42d88f742e97e0cfd
deStarxis <humagainuzjwol@gmail.com>
2023-02-02 22:49:23 -0600

    backend initialization

---
+++ rental-app-server/database/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+module.exports.databaseConnection = async () => {
+  try {
+    mongoose.set("strictQuery", true);
+    const result = await mongoose.connect(
+      process.env.MONGO_URI || process.env.LOCAL_DATABASE_URI,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log(`MongoDB connected to : ${result.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ rental-app-server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const dotenv = require("dotenv");
+const path = require("path");
+const morgan = require("morgan");
+const cors = require("cors");
+const { databaseConnection } = require("./database/database");
+dotenv.config({
+  path: "./.env",
+});
+const app = express();
+app.use(cors());
+app.use(morgan("dev"));
+app.use("/assets/pics", express.static(path.join(__dirname, "assets/pics")));
+app.use(express.json());
+
+databaseConnection();
+app.use(
+  router.get("/", (req, res) => {
+    res.status(200).json({
+      success: true,
+      message: "Welcome to Home Page",
+    });
+  })
+);
+// app.use("/api/users", );
+
+app.all("*", (req, res, next) => {
+  next(new Error(`Route Not found`));
+});
+
+app.use((error, req, res, next) => {
+  res.status(400).json({ success: false, error: error.message });
+});
+
+const PORT = process.env.PORT || 80;
+app.listen(PORT, console.log(`Running on port : ${PORT}`));
