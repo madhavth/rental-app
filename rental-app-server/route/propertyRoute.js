bb4c491d9e78cc909ea20f590df830fe3144ac1a
madhavth <madhavth77@gmail.com>
2023-02-04 10:27:13 -0600

    Added missing routes

--- rental-app-server/route/propertyRoute.js
+++ rental-app-server/route/propertyRoute.js
@@ -1,18 +1,26 @@
 const express = require("express");
-const { upload } = require("../config/multer");
+const {upload} = require("../config/multer");
 const router = express.Router();
 const {
-  getAllProperties,
-  getPropertyById,
-  deletePropertyById,
-  addProperty,
-  updateProperty,
+    getAllProperties,
+    getPropertyById,
+    deletePropertyById,
+    addProperty,
+    updateProperty,
+    getNearByProperties,
+    getTrendingProperties,
+    uploadPropertyImages,
 } = require("../controller/propertyController");
 
 router.get("/", getAllProperties);
+router.get("/nearby", getNearByProperties);
+router.get("/trending", getTrendingProperties);
+
 router.get("/:property_id", getPropertyById);
 router.delete("/:property_id", deletePropertyById);
 router.post("/", addProperty);
 router.patch("/:property_id", updateProperty);
+
+router.patch("/upload-property-images", upload.array("image", 5), uploadPropertyImages);
 
 module.exports = router;
