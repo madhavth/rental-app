6fad0e79f27f3109f396ed9eb00cb15457f98605
madhavth <madhavth77@gmail.com>
2023-02-05 17:51:20 -0600

    added overall_rating when added, removed reviews

--- rental-app-server/controller/reviewsController.js
+++ rental-app-server/controller/reviewsController.js
@@ -1,5 +1,34 @@
 // GET, POST, PATCH, DELETE for reviews
 const Property = require("../model/propertyModel");
+const mongoose = require("mongoose");
+
+const recalculateRating = async (property_id) => {
+  const results = await Property.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(property_id),
+      },
+    },
+    { $unwind: "$reviews" },
+    {
+      $group: {
+        _id: { _id: "$_id" },
+        rating: {
+          $avg: "$reviews.rating",
+        },
+      },
+    },
+  ]);
+
+  if (results.length > 0) {
+    const result = await Property.updateOne(
+      { _id: property_id },
+      { overall_rating: results[0].rating }
+    );
+  }
+
+  return results;
+};
 
 module.exports.getAllReviews = async (req, res, next) => {
   try {
@@ -20,6 +49,9 @@ module.exports.removeReviewById = async (req, res, next) => {
       { _id: req.params.property_id },
       { $pull: { reviews: { _id: req.params.review_id } } }
     );
+
+    await recalculateRating(req.params.property_id);
+
     res.json({ success: true, message: "Deleted review successfully" });
   } catch (e) {
     next(new Error("Error while deleting review"));
@@ -33,6 +65,8 @@ module.exports.addReview = async (req, res, next) => {
       { $push: { reviews: req.body } }
     );
 
+    const newRes = await recalculateRating(req.params.property_id);
+
     const success = result.modifiedCount !== 0;
 
     res.json({
@@ -40,6 +74,7 @@ module.exports.addReview = async (req, res, next) => {
       message: !success ? "Failed to add review" : "Added review successfully",
     });
   } catch (e) {
+    console.log(e);
     next(new Error("Error while adding review"));
   }
 };
