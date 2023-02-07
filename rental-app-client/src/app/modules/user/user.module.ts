8845e2edf41d5e6ac9389b631a7b7a80f7c9feae
madhavth <madhavth77@gmail.com>
2023-02-07 16:48:28 -0600

    login failed, success loading and message shown

--- rental-app-client/src/app/modules/user/user.module.ts
+++ rental-app-client/src/app/modules/user/user.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from 'src/app/pages/login/login.component';
 import { SignupComponent } from 'src/app/pages/signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import {HomeModule} from "../home/home.module";
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
@@ -11,9 +12,10 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     ReactiveFormsModule,
     RouterModule.forChild([
-      { path: 'login', component: LoginComponent },
-      { path: 'signup', component: SignupComponent },
+      {path: 'login', component: LoginComponent},
+      {path: 'signup', component: SignupComponent},
     ]),
+    HomeModule,
   ],
 })
 export class UserModule {}
--- rental-app-client/src/app/pages/login/login.component.ts
+++ rental-app-client/src/app/pages/login/login.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import jwtDecode from 'jwt-decode';
 import { User } from 'src/app/model/user';
 import { UserService } from 'src/app/services/user.service';
+import {catchError, of} from "rxjs";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-login',
@@ -11,6 +13,9 @@ import { UserService } from 'src/app/services/user.service';
   styles: [],
 })
 export class LoginComponent {
+  isLoggingIn = false;
+
+  toastService = inject(ToastrService);
   userService = inject(UserService);
   router = inject(Router);
   loginForm = inject(FormBuilder).nonNullable.group({
@@ -18,9 +23,21 @@ export class LoginComponent {
     password: ['', Validators.required],
   });
   login() {
-    this.userService.login(this.loginForm.value as User).subscribe((res) => {
+    this.isLoggingIn = true;
+
+    this.userService.login(this.loginForm.value as User).pipe(
+      catchError((err)=> {
+        this.isLoggingIn = false;
+        return of({success: false, data: err});
+      })
+    ).subscribe((res) => {
+      this.isLoggingIn = false;
       if (res.success) {
         const decoded: User = jwtDecode(res.data);
+
+        this.toastService.success(`Welcome, ${decoded.firstName} ${decoded.lastName}` , 'Logged In Successfully',
+          );
+
         this.userService.setUserState({
           token: res.data,
           email: decoded.email,
@@ -34,6 +51,9 @@ export class LoginComponent {
           })
         );
         this.router.navigate(decoded.role === 'admin' ? ['', 'admin'] : ['']);
+      }
+      else {
+        this.toastService.error("Invalid Credentials", "Login Failed");
       }
     });
   }
